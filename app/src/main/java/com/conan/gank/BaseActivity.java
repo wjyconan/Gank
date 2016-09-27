cf3f76f8479b1de35addaf16ffb732e10a1844e3
conan <673665678@qq.com>
2016-09-27 18:13:57 +0800

    update

---
+++ app/src/main/java/com/conan/gank/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.conan.gank;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by conan on 16/9/27.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
--- app/src/main/java/com/conan/gank/MainActivity.java
+++ app/src/main/java/com/conan/gank/MainActivity.java
@@ -1,13 +1,19 @@
 package com.conan.gank;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.conan.gank.bean.User;
+import com.conan.gank.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Test", "User");
+        binding.setUser(user);
     }
 }
